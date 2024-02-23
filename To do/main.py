63269ea6d4784d9338a5a56bb02b080ba7f2632a
Pramish K.C <pramishkc066@gmail.com>
2024-02-23 14:06:14 +0545

    completed To do

--- To do/main.py
+++ To do/main.py
@@ -2,17 +2,33 @@ tasks = []
 print("Stay on track, get it done! Your ultimate to-do list guide.")
 
 
-def addtask(tasks):
+def add_task():
     task = input("Enter a task: ")
     tasks.append(task)
     print(f"Task {task} added to the list")
 
-def deletetask(task):
-     #displaytask()
+
+def display_task():
+    if not tasks:
+        print("There are no current task")
+    else:
+        for index, task in enumerate(tasks):
+            print(f"{index}. {task}")
+
+
+def delete_task():
+    display_task()
     try:
         task_to_delete = int(input("No. of task to delete: "))
+        if task_to_delete >= 0 and task_to_delete < len(tasks):
+            tasks.pop(task_to_delete)
+            print(f"Task {task_to_delete} has been removed")
+        else:
+            print(f"Task {task_to_delete} not found")
     except:
         print("Invalid Input")
+
+
 while True:
     print("Please select one of the following option")
     print("-----------------------------------------")
@@ -23,11 +39,11 @@ while True:
 
     choice = int(input("Enter your choice: "))
     if choice == 1:
-        addtask(tasks)
+        add_task()
     elif choice == 2:
-        #deletetask()
+        delete_task()
     elif choice == 3:
-        #displaytask()
+        display_task()
     elif choice == 4:
         break
     else:
