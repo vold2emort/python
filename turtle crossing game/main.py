1095014908f7f2e9ca51f278df4671370a345591
Pramish K.C <pramishkc066@gmail.com>
2024-03-03 17:48:45 +0545

    completed

--- turtle crossing game/main.py
+++ turtle crossing game/main.py
@@ -7,8 +7,9 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-car_manager = CarManager()
 
+car_manager = CarManager()
+scoreboard = Scoreboard()
 player_turtle = Player()
 
 screen.listen()
@@ -21,3 +22,15 @@ while game_is_on:
     screen.update()
     car_manager.create_car()
     car_manager.move_cars()
+    scoreboard.display_level()
+    for car in car_manager.all_cars:
+        if car.distance(player_turtle) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    if player_turtle.is_at_finish():
+        player_turtle.restore_position()
+        car_manager.level_up()
+        scoreboard.update_level()
+
+screen.exitonclick()
