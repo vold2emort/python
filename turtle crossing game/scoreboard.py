bb2508327f5297f4bc7e6e0efa393796dbfb440b
Pramish K.C <pramishkc066@gmail.com>
2024-03-03 17:49:44 +0545

    completed

--- turtle crossing game/scoreboard.py
+++ turtle crossing game/scoreboard.py
@@ -1,5 +1,22 @@
+import turtle
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(turtle.Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level = 1
+
+    def display_level(self):
+        self.clear()
+        self.goto(-280, 250)
+        self.write(arg=f"Level:{self.level}", align="left", font=FONT)
+
+    def update_level(self):
+        self.level += 1
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(arg="GAME OVER!!", align="center", font=FONT)
